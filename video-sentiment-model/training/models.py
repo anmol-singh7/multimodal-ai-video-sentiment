cdf8f961f119b602bf73e0ad8d97297542c4e71e
anmol-singh7 <singhanmol0984@gmail.com>
2025-08-01 17:14:18 +0530

    implemented videoEncoder

--- video-sentiment-model/training/models.py
+++ video-sentiment-model/training/models.py
@@ -20,4 +20,25 @@ class TextEncoder(nn.Module):
         pooler_output = outputs.pooler_output
 
         return self.projection(pooler_output)
+
+
+class VideoEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.backbone = vision_models.video.r3d_18(pretrained = True)
+
+        for param in self.backbone.parameters():
+            param.requires_grad = False
+
+        nums_fts = self.backbone.fc.in_features
+        self.backbone.fc = nn.Sequential(
+            nn.Linear(nums_fts,128),
+            nn.ReLU(),
+            nn.Dropout(0.2)
+        )
     
+    def forward(self, x):
+        # [batch_size, frames, channels, height, width] -> [batch_size, channels, frames, height, width] 
+        x = x.transpose(1,2)
+        
+        return self.backbone(x)
